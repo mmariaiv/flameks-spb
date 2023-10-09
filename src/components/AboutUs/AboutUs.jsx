b161aedd1b135536fc31beada5b93fab67a71f54
mmariaiv <mari.ivashova@gmail.com>
2023-10-09 20:58:11 +0300

    final_4.0

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { HashLink as Link } from "react-router-hash-link";
 
 function AboutUs() {
 	return (
@@ -6,25 +7,34 @@ function AboutUs() {
 			<div className="about-us__description">
 				<h2 className="about-us__title title">О нас</h2>
 				<p className="about-us__advert">
-					Фламекс&nbsp;&mdash; инженерная компания в&nbsp;Санкт-Петербурге,
-					предлагающая комплексные услуги по&nbsp;проектированию, монтажу
-					и&nbsp;обслуживанию инженерных систем для зданий и&nbsp;сооружений.
-					Мы&nbsp;используем современные материалы, технологии и&nbsp;строго
-					следим за&nbsp;соблюдением всех норм и&nbsp;регламентов для гарантии
-					высокого качества и&nbsp;надежности ваших проектов.
+					Фламекс&nbsp;СПБ&nbsp;&mdash; инженерная компания
+					в&nbsp;Санкт-Петербурге, предлагающая комплексные услуги
+					по&nbsp;проектированию, монтажу и&nbsp;обслуживанию инженерных систем
+					для зданий и&nbsp;сооружений. Мы&nbsp;используем современные
+					материалы, технологии и&nbsp;строго следим за&nbsp;соблюдением всех
+					норм и&nbsp;регламентов для гарантии высокого качества
+					и&nbsp;надежности ваших проектов.
 				</p>
 			</div>
 			<div className="about-us__service">
 				<h2 className="about-us__title title">Услуги</h2>
-				<div className="about-us__list">
-					<p className="about-us__list-item">Строительство</p>
+				<nav className="about-us__list">
+					<Link to="#building" className="about-us__list-item" smooth>
+						Строительство
+					</Link>
 					<hr className="border" />
-					<p className="about-us__list-item">Поставка оборудования</p>
+					<Link to="#delivery" className="about-us__list-item" smooth>
+						Поставка оборудования
+					</Link>
 					<hr className="border" />
-					<p className="about-us__list-item">Обслуживание</p>
+					<Link to="#handling" className="about-us__list-item" smooth>
+						Обслуживание
+					</Link>
 					<hr className="border" />
-					<p className="about-us__list-item">Планирование</p>
-				</div>
+					<Link to="#planning" className="about-us__list-item" smooth>
+						Планирование
+					</Link>
+				</nav>
 			</div>
 		</section>
 	);
--- src/components/ServiceCard/ServiceCard.jsx
+++ src/components/ServiceCard/ServiceCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function ServiceCard(props) {
 	return (
-		<div className="service-item">
+		<div className="service-item" id={props?.id}>
 			<div className="service-item__description">
 				<h3 className="service-item__title title">{props.title}</h3>
 				<p className="service-item__about">{props.description}</p>
